8a1c6b3d45b75f36fb65ec7d56b75b9a578d50d7
rochelryu <core.irie@gmail.com>
2019-05-29 15:38:59 +0000

    Send 1/2

--- js/app.js
+++ js/app.js
@@ -42,3 +42,57 @@ function shiftRight() {
     }, 500);
 
 }
+function someFunction(destroyFeedback) {
+    // Do your stuff here
+    // Call destroyFeedback() function when you're done
+    // The true parameter will proceed to the next step besides destroying the preloader
+    setTimeout(() => {
+        destroyFeedback(true);
+    }, 500);
+}
+function anyThing(destroyFeedback) {
+    setTimeout(function(){ destroyFeedback(true); }, 1500);
+}
+
+function noThing(destroyFeedback) {
+    setTimeout(function(){ destroyFeedback(true); }, 10000);
+}
+
+var stepperDiv = document.querySelector('.stepper');
+console.log(stepperDiv);
+var stepper = new MStepper(stepperDiv);
+
+var tra = 1;
+
+function augmente(ele){
+    var el = parseInt(ele.parentElement.children[0].children[1].children[0].innerHTML.substring(1, ele.parentElement.children[0].children[1].children[0].innerHTML.length), 10);
+    var price = parseInt(ele.parentElement.children[0].children[1].children[1].innerHTML, 10);
+    ele.parentElement.children[0].children[1].children[0].innerHTML = "+" + (el + 1);
+    ele.parentElement.children[0].children[0].children[1].innerHTML = "" + (price * (el + 1)) + " F cfa";
+}
+
+function diminue(ele){
+    var price = parseInt(ele.parentElement.children[0].children[1].children[1].innerHTML, 10);
+    var el = parseInt(ele.parentElement.children[0].children[1].children[0].innerHTML.substring(1, ele.parentElement.children[0].children[1].children[0].innerHTML.length), 10);
+    if(el > 0) {
+        ele.parentElement.children[0].children[1].children[0].innerHTML = "+" + (el - 1);
+        ele.parentElement.children[0].children[0].children[1].innerHTML = "" +  (price * (el - 1)) + " F cfa";
+    }
+}
+function nextStep(ele) {
+    ele.parentElement.children[ele.parentElement.childElementCount - 1].scrollLeft += ele.parentElement.children[ele.parentElement.childElementCount - 1].children[0].clientWidth
+}
+function prevStep(ele) {
+    ele.parentElement.children[ele.parentElement.childElementCount - 1].scrollLeft -= ele.parentElement.children[ele.parentElement.childElementCount - 1].children[0].clientWidth
+}
+function focuss(ele) {
+    if(tra == 1){
+        ele.className = "shower";
+        tra = 0
+    }
+    else{
+        tra = 1;
+        ele.className = "hiden";
+    }
+
+}
